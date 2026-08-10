d7d86f6b6c52c02ee3c61fa77cc079d2da0fe2c6
boldt <boldt@DESKTOP-8C3CGTV>
2026-08-10 11:01:41 +0800

    加入專案檔案。

---
+++ YoutubeApp/Contracts/MainContract.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YoutubeApp.Contracts
+{
+    internal class MainContract
+    {
+        public interface IMainView { }
+        public interface IMainPresenter { }
+    }
+}
